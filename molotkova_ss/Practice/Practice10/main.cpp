a0d69b0678caa0188732c7892309419b3900f5b2
Svetlana Molotkova <42940205+SMolotkova@users.noreply.github.com>
2019-06-01 10:36:24 +0300

    Update main.cpp

--- molotkova_ss/Practice/Practice10/main.cpp
+++ molotkova_ss/Practice/Practice10/main.cpp
@@ -12,33 +12,19 @@ void main()
 	cout << "Count = ";
 	cin >> count;
 	Container1<int> Container_1(MAXSIZE);
-	cout << "Container1(Fill) = " << endl;
-	try 
-	{
-		Container_1.Fill(count);//FILLING
-	}
-	catch (const char* error)
-	{
-		cout << error << endl;
-	}
-	Container_1.Print();//OUTPUT
+	cout << "Container1 = " << endl;
+        cin  >> Container_1 >> endl;
+	cout << "Container1(Fill) = " << Container_1 << endl;
 	Container1<int> COPYContainer_1(Container_1);
 	COPYContainer_1.Add(5);
-	COPYContainer_1.Print();
+	cout << "COPYContainer_1 = " << COPYContainer_1 << endl;	
 	COPYContainer_1.Add(12);
-	COPYContainer_1.Print();
-	try
-	{
-		COPYContainer_1.Fill(0);
-	}
-	catch (const char* error)
-	{
-		cout << error << endl;
-	}
+	cout << "COPYContainer_1 = " << COPYContainer_1 << endl;
+	cin  >> COPYContainer_1 >> endl;
 	COPYContainer_1.IsEmpty();
 	try
 	{ 
-		Container_1.Delete(4);
+		COPYContainer_1.Delete(4);
 	}
 	catch (const char* error)
 	{
@@ -52,31 +38,17 @@ void main()
 	cout << "Count = ";
 	cin >> count;
 	Container1<int*> Container_2(MAXSIZE);
-	cout << "Container2(Fill) = " << endl;
-	try
-	{
-		Container_2.Fill(count);//FILLING
-	}
-	catch (const char* error)
-	{
-		cout << error << endl;
-	}
-	Container_2.Print();//OUTPUT
+	cout << "Container2 = " << endl;
+	cin  >> Container_2 >> endl;
+	cout << Container_2 << endl;//OUTPUT
 	Container1<int*> COPYContainer_2(Container_2);
 	int a = 5;
 	COPYContainer_2.Add(&a);
-	COPYContainer_2.Print();
+	cout << "COPYContainer_2 = " << COPYContainer_2 <<endl;
 	int b = 12;
 	COPYContainer_2.Add(&b);
-	COPYContainer_2.Print();
-	try
-	{
-		COPYContainer_2.Fill(0);
-	}
-	catch (const char* error)
-	{
-		cout << error << endl;
-	}
+	cout << "COPYContainer_2 = " << COPYContainer_2 <<endl;
+	cin  >> COPYContainer_2 >> endl;
 	COPYContainer_2.IsEmpty();
 	try
 	{
